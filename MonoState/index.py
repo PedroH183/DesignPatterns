521dac4d556d90f262d3f859e1174291da7264cd
PedroH183 <pedro_freires18@outlook.com>
2024-01-03 18:27:58 -0500

    feat: file run

---
+++ MonoState/index.py
@@ -0,0 +1,17 @@
+
+from MonoState.UserModule.User import User
+from MonoState.UsuariosCadastradosModule.UsuariosCadastrados import UsuariosCadastrados
+
+if __name__ == "__main__":
+    
+    user_one = User("Pedro")
+    user_two = User("John")
+    user_tree = User("Gabriel")
+    
+    list_one_users = UsuariosCadastrados()
+    list_one_users.setUsers([user_one, user_two])
+    print(list_one_users)
+    
+    list_two_users = UsuariosCadastrados()
+    list_two_users.setUsers(user_tree)
+    print(list_one_users) # é a mesma que a da linha 14
