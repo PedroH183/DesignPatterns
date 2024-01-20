bfdf577c8b92540e1af07518578c7a2f2fc12075
PedroH183 <pedro_freires18@outlook.com>
2024-01-19 22:51:33 -0500

    docs: typing objects

---
+++ Adapter /AdapterExample/interfaces.ts
@@ -0,0 +1,27 @@
+
+export type asciiToDecimal = {
+  "48": number;
+  "49": number;
+  "50": number;
+  "51": number;
+  "52": number;
+  "53": number;
+  "54": number;
+  "55": number;
+  "56": number;
+  "57": number;
+}
+
+
+export interface user{
+  nome : string,
+  senha : string,
+}
+
+export interface IAuthentication{
+  login( username : string, password : string ) : {}
+}
+
+export interface AdapterAuthenticationAlgorithm{
+  getPasswordInDecimal( password : string ): string
+}
