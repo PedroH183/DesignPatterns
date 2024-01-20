aded003db4e4a4047354fa9d5fdf1cd936338325
PedroH183 <pedro_freires18@outlook.com>
2024-01-19 22:50:41 -0500

    feat : implementing my example of adapter

--- Adapter /AdapterExample/classes.ts
+++ Adapter /AdapterExample/classes.ts
@@ -1,20 +1,57 @@
-import { IAuthentication, user } from "./interfaces";
+import { AdapterAuthenticationAlgorithm, IAuthentication, asciiToDecimal, user } from "./interfaces";
 
-const db_users : user[] = [
+
+export const db_users : user[] = [
+  // password pattern > method:password 
+  // consideração a senha é composta apenas de numeros
   {
     "nome" : "Pedro",
-    "senha": "1234",
+    "senha": "decimal:1234", // method: ascii and password : 48 49 50 51 52
   },
   {
     "nome" : "Arnold",
-    "senha": "4321"
+    "senha": "decimal:1234" // method: decimal and password : '1234'
   }
 ]
 
+export class AdapterAthentication implements AdapterAuthenticationAlgorithm{
+  
+  mapper_numbers_in_ascii : {[key : string ] : number} = {
+    "48" : 0,
+    "49" : 1,
+    "50" : 2,
+    "51" : 3,
+    "52" : 4,
+    "53" : 5,
+    "54" : 6,
+    "55" : 7,
+    "56" : 8,
+    "57" : 9,
+  }
 
-class authentication implements IAuthentication{
+  getPasswordInDecimal( passwordInAsciiComplete : string ): string {
+    let passwordInDecimal : string[] = []
+    let passwordInAscii = passwordInAsciiComplete.split(":")[1].split(" ")
+
+    for( let i = 0; i < passwordInAscii.length; ++i ){
+      passwordInDecimal.push( 
+        this.mapper_numbers_in_ascii[ passwordInAscii[i] ].toString()
+      );
+    }  
+    return "decimal:" +  passwordInDecimal.join("");
+  }
+}
+
+
+export class authenticationService implements IAuthentication{
   /*
     Classe responsável por fazer a autenticação
+    
+    O client não pode se comunicar diretamente com esta classe pois há 
+    diferentes tipos de criptografia no sistema, sendo necessário passar por
+    uma adaptação.
+
+    Esse serviço só funciona com password em decimal !
   */
   
   login(username: string, password: string): {} {
@@ -25,7 +62,7 @@ class authentication implements IAuthentication{
       return {"message" : "usuario não encontrado", "error" : true}
     }
 
-    const password_check_result = this._checkPassword(password)
+    const password_check_result = this._checkPassword( user as user, password)
 
     if( password_check_result === false ){
       return {"message" : "credenciais incorretas", "error" : true}
@@ -34,19 +71,20 @@ class authentication implements IAuthentication{
     return {"message" : "usuario logado", "error" : false}
   }
 
-  private _findUsernameIfExist( username : string ): boolean  {
+  private _findUsernameIfExist( username : string ): boolean | {}  {
     
     const user_exist = db_users.filter( 
       (usuario : user) => { return( usuario["nome"] === username ) }
     )
-    
-    return user_exist.length > 0
+    return user_exist.length > 0 ? user_exist[0] : false
   }
 
-  private _checkPassword(password: string): boolean {
+  private _checkPassword( user : user, password: string ): boolean {
 
-
-
-    throw new Error("Method not implemented.");
+    // restrict to allow only decimal methods
+    if( password.split(":")[1] === "ascii" ){
+      return false
+    }
+    return user["senha"] === password
   }
 }
